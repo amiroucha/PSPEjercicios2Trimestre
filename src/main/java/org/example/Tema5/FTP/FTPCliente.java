cc8e5a0bf5585ddd30fd9ab434f5023c84f0ed13
Chaima <1925046@alu.murciaeduca.es>
2025-02-11 11:59:50 +0100

    Ejercicio FTP hecho

--- src/main/java/org/example/Tema5/FTP/FTPCliente.java
+++ src/main/java/org/example/Tema5/FTP/FTPCliente.java
@@ -1,69 +1,93 @@
 package org.example.Tema5.FTP;
 
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
 import org.apache.commons.net.ftp.FTPReply;
-import org.example.Tema6.Act2.ClienteTCP;
-
 import java.io.IOException;
-import java.net.SocketException;
 import java.util.Scanner;
 
 public class FTPCliente {
     //objeto de la clase FTPClient
     private static FTPClient clienteFTP;
     //usuario
-    private static String usuario = "anonymous";
+    private static final String usuario = "chai";
     //contraseña
-    private static String password = "";
-
+    private static final String password = "admin";
+    private static final String servidor = "localhost";
 
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
         try{
-            int respuesta; //va a ser la respuesta del servidor
-            //creación del objeto cliente FTP
-            clienteFTP = new FTPClient();
-            //conexión del cliente al servidor FTP
-            clienteFTP.connect("ftp.rediris.es");
+            clienteFTP = new FTPClient();   //creación del objeto cliente FTP
+
+            clienteFTP.connect(servidor);//conexión del cliente al servidor FTP
             //Comprobación de la conexión
-            respuesta = clienteFTP.getReplyCode();
             //si la conexión  se completa ok
-            if (FTPReply.isPositiveCompletion(respuesta)) {
-                //abre una sesión con el usuario anónimo
-                clienteFTP.login(usuario, password);
-                //Activar el modo pasivo para no tener problemas con el Firewall de Windows
-                clienteFTP.enterLocalPassiveMode();
-                System.out.println("Introduce el nombre del directorio deseado");
-                String directorio = leer.nextLine();
+            if (FTPReply.isPositiveCompletion(clienteFTP.getReplyCode())) {
+                System.out.println(clienteFTP.getReplyString()); //imprime la respuesta del servidor
+                //abre una sesión con el usuario
+                if(clienteFTP.login(usuario, password)){
+                    System.out.println(clienteFTP.getReplyString()); //imprime la respuesta del servidor
+                    System.out.println("Sesión iniciada correctamente");
 
-                if (!clienteFTP.changeWorkingDirectory(directorio)) {
-                    System.out.println("No existe el directorio " + directorio+"\nCreando lo...");
-                    if(clienteFTP.makeDirectory(directorio)){
-                        System.out.println("Directorio creado correctamente");
-                    }else{
-                        System.out.println("Directorio no se ha podido crear");
+                    //Activar el modo pasivo para no tener problemas con el Firewall de Windows
+                    clienteFTP.enterLocalPassiveMode();
+                    /*-------------------------------------------------*/
+                    System.out.println("Listo los directorios disponibles");
+                    System.out.println("Lista de directorios accesibles:");
+                    FTPFile[] archivosRaizDisponible = clienteFTP.listFiles();
+                    for (FTPFile archivo : archivosRaizDisponible) {
+                        if (archivo.isDirectory()) {
+                            System.out.println("📂 " + archivo.getName());
+                        }
                     }
+                    /*-------------------------------------------------*/
+
+                    System.out.println("Introduce el nombre del directorio deseado");
+                    String directorio = leer.nextLine();
+
+                    if (!clienteFTP.changeWorkingDirectory(directorio)) {
+                        System.out.println("No existe el directorio " + directorio+"\nCreando...");
+                        if(clienteFTP.makeDirectory(directorio)){
+                            System.out.println("Directorio creado correctamente");
+                        }else{
+                            System.out.println("Directorio no se ha podido crear");
+                        }
+                    }
+
+                    clienteFTP.changeWorkingDirectory(directorio);
+                    System.out.println("Contenido del directorio " + directorio + ":");
+
+                    FTPFile[] archivos = clienteFTP.listFiles();
+                    for (FTPFile archivo : archivos) {
+                        System.out.println(archivo.getName());
+                    }
+                    //cerrar sesion del usuario
+                    clienteFTP.logout();
+                }else{
+                    System.out.println(clienteFTP.getReplyString()); //imprime la respuesta del servidor
+                    System.out.println("Error en la autenticación");
                 }
-                //me quedo en comprobar lo del directorio y si no crearlo
-                //me falta listar el contenido como pde mariajo
-
-
-
 
             } else {
                 //desconecta
-                clienteFTP.disconnect();
+                System.out.println(clienteFTP.getReplyString()); //imprime la respuesta del servidor
                 System.err.println("FTP ha rechazado la conexión esblecida");
-                System.exit(1);
             }
-
-
-
-
-
 
         } catch (IOException e) {
             System.err.println("FTP client error: " + e.getMessage());
+            System.out.println(clienteFTP.getReplyString()); //imprime la respuesta del servidor
+        }finally {
+            if (clienteFTP.isConnected()) {
+                try {
+                    clienteFTP.disconnect();
+                    System.out.println("Respuesta Servidor "+clienteFTP.getReplyString()); //imprime la respuesta del servidor
+                    System.out.println("Conexión cerrada");
+                } catch (IOException e) {
+                    System.err.println("Error al cerrar la conexión: " + e.getMessage());
+                }
+            }
         }
 
 
